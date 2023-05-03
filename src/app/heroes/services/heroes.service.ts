a6a2795d593dcd711672bc68ee11e0f833690d67
Calite <practicas.soporte@icp.es>
2023-05-03 09:40:07 +0200

    03.05.23

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -22,8 +22,28 @@ export class HeroesService {
             );
     }
 
-    getSuggestions(query : string) : Observable<Hero[]> {
+    getSuggestions(query: string): Observable<Hero[]> {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes?=${query}&_limit=5`);
     }
+
+    addHero(hero: Hero): Observable<Hero> {
+        return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero)
+    }
+
+
+    updateHero(hero: Hero): Observable<Hero> {
+        if (!hero.id) throw Error('Hero id is required');
+        return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero)
+    }
+
+
+    deleteHeroById(id: string): Observable<boolean> {
+        return this.http.delete<Hero>(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+                map(response => true),
+                catchError(err => of(false)),
+            )
+    }
+
 
 }
